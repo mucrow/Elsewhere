0977801333a33a39dbb589e1f20bb182b74daec6
mucrow <120820315+mucrow@users.noreply.github.com>
2023-08-13 20:04:19 -0400

    Fix smoothing on rotation and velocity on Player

--- Elsewhere/Assets/Scripts/Player/Player.cs
+++ Elsewhere/Assets/Scripts/Player/Player.cs
@@ -10,6 +10,7 @@ namespace Elsewhere.Player {
     Input _input;
     Interactor _interactor;
     Rigidbody _rigidbody;
+    Vector3 _velocity;
 
     void Awake() {
       _characterController = GetComponent<CharacterController>();
@@ -27,20 +28,20 @@ namespace Elsewhere.Player {
 
     void HandleMoveInput(float dt) {
       if (_input.Move.magnitude < 0.1f) {
-        _characterController.SimpleMove(Vector3.zero);
+        UpdateCharacterControllerVelocity(dt, Vector3.zero);
       }
       else {
         var localRotation = transform.localRotation;
         var moveInputQuaternion = Quaternion.LookRotation(new Vector3(_input.Move.x, 0f, _input.Move.y), Vector3.up);
-        var newRotation = Quaternion.RotateTowards(localRotation, moveInputQuaternion, _rotateSpeed * Time.fixedDeltaTime);
+        var newRotation = Quaternion.RotateTowards(localRotation, moveInputQuaternion, _rotateSpeed * dt);
         transform.localRotation = newRotation;
 
         if (Quaternion.Angle(newRotation, moveInputQuaternion) < 0.5f) {
           var targetVelocity = newRotation * Vector3.forward * _moveSpeed;
-          _characterController.SimpleMove(targetVelocity);
+          UpdateCharacterControllerVelocity(dt, targetVelocity);
         }
         else {
-          _characterController.SimpleMove(Vector3.zero);
+          UpdateCharacterControllerVelocity(dt, Vector3.zero);
         }
       }
     }
@@ -49,6 +50,15 @@ namespace Elsewhere.Player {
       if (_input.Interact) {
         _interactor.Interact();
       }
+    }
+
+    void UpdateCharacterControllerVelocity(float dt, Vector3 targetVelocity) {
+      if (targetVelocity.y != 0f) {
+        Debug.LogWarning("targetVelocity with Y-component not yet supported");
+        targetVelocity.y = 0f;
+      }
+      _velocity = Vector3.MoveTowards(_velocity, targetVelocity, _moveAcceleration * dt);
+      _characterController.SimpleMove(_velocity);
     }
 
     void UpdateRigidbodyVelocity(float dt, Vector3 targetVelocity) {
