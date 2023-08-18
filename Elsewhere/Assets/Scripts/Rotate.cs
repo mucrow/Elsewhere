e4f5607a0bf06b9d6526e7c3fbd537e66e35b904
mucrow <120820315+mucrow@users.noreply.github.com>
2023-08-17 20:34:25 -0400

    Add portal placeholder object

---
+++ Elsewhere/Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Elsewhere {
+  public class Rotate: MonoBehaviour {
+    public bool YClockwise = true;
+    public float CycleDuration = 2f;
+
+    void Update() {
+      float dt = Time.deltaTime;
+      float direction = YClockwise ? 1f : -1f;
+      var angles = transform.localEulerAngles;
+      angles.y += (360f / CycleDuration) * direction * dt;
+      transform.localEulerAngles = angles;
+    }
+  }
+}
